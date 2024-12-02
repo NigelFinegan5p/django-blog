a4ebdb32e938375986ebe0409a69f4c5c5d53b9f
Nigel Finegan <fvunpiqh@students.codeinstitute.net>
2024-12-02 15:45:37 +0000

    runtime.txt

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nigelfinegan-djangoblog-5lmc7m69ap7.ws.codeinstitute-ide.net','.herokuapp.com']
 
